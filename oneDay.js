06e5869a5554fc452d0f304ac35fa3d49573d24c
Ferdi Ahcıoğlu <ferdiahcioglu@gmail.com>
2022-12-27 23:06:54 +0300

    oneDay

---
+++ oneDay.js
@@ -0,0 +1,17 @@
+//console.log('hello word ')
+console.log( typeof 'Ferdi Ahcıoğlu')
+console.log( typeof 'Bekar')
+console.log( typeof 'Turkey')
+console.log( typeof 24 )
+console.log( typeof true)
+console.log( typeof null)
+console.log( typeof undefined)
+
+let name =  'Ferdi Ahcıoğlu';
+let userAge = 24;
+const country = 'Turkey';
+let maritalStatus = 'Bekar';
+/*
+  let = değişken değerinin farklı yerde değiştirilmesi gerekiyorsa let kullanılır
+  const = değişken verisi hiçbir yerde değişmemesi gerekiyorsa const kullanılır*/
+console.log (name, userAge, country, maritalStatus )
